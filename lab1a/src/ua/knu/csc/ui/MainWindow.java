44f988bbfb2f3e487d675872819c2fc00f5f4cc8
Timur Krasnopir <timur.krasnopir@unicyb.kiev.ua>
2021-02-04 01:02:46 +0200

    Add spinners and labels

--- lab1a/src/ua/knu/csc/ui/MainWindow.java
+++ lab1a/src/ua/knu/csc/ui/MainWindow.java
@@ -1,6 +1,7 @@
 package ua.knu.csc.ui;
 
 import javax.swing.*;
+import javax.swing.border.TitledBorder;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 import java.awt.*;
@@ -14,7 +15,29 @@ public class MainWindow extends JFrame implements ChangeListener {
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLayout(new FlowLayout());
 
-        add(new JButton("Start"));
+        JPanel labeledSpinner1 = new JPanel(new FlowLayout());
+        labeledSpinner1.add(new JLabel("Thread 1:"));
+        labeledSpinner1.add(new JSpinner(new SpinnerNumberModel(Thread.NORM_PRIORITY, Thread.MIN_PRIORITY, Thread.MAX_PRIORITY, 1)));
+
+        JPanel labeledSpinner2 = new JPanel(new FlowLayout());
+        labeledSpinner2.add(new JLabel("Thread 2:"));
+        labeledSpinner2.add(new JSpinner(new SpinnerNumberModel(Thread.NORM_PRIORITY, Thread.MIN_PRIORITY, Thread.MAX_PRIORITY, 1)));
+
+        Box labeledSpinnersBox = Box.createVerticalBox();
+        labeledSpinnersBox.setBorder(new TitledBorder("Priority"));
+        labeledSpinnersBox.add(labeledSpinner1);
+        labeledSpinnersBox.add(labeledSpinner2);
+
+        Box controlPanelBox = Box.createVerticalBox();
+        controlPanelBox.add(labeledSpinnersBox);
+        controlPanelBox.add(Box.createVerticalStrut(10));
+
+        JButton buttonStart = new JButton("Start");
+        buttonStart.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        controlPanelBox.add(buttonStart);
+
+        add(controlPanelBox);
 
         slider.setPaintTicks(true);
         slider.setPaintLabels(true);
