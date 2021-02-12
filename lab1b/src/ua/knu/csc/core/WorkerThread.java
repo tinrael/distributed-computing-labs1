74df6401437cd0b4c4209b5e86320113f94b9cb2
Timur Krasnopir <timur.krasnopir@unicyb.kiev.ua>
2021-02-13 01:13:16 +0200

    Rename the Worker class

--- lab1b/src/ua/knu/csc/core/WorkerThread.java
+++ lab1b/src/ua/knu/csc/core/WorkerThread.java
@@ -2,17 +2,17 @@ package ua.knu.csc.core;
 
 import javax.swing.JSlider;
 
-public class Worker extends Thread {
+public class WorkerThread extends Thread {
     private final BinarySemaphore binarySemaphore;
 
     private final JSlider slider;
 
-    public Worker(BinarySemaphore binarySemaphore, JSlider slider) {
+    public WorkerThread(BinarySemaphore binarySemaphore, JSlider slider) {
         this.binarySemaphore = binarySemaphore;
         this.slider = slider;
     }
 
-    public Worker(String name, BinarySemaphore binarySemaphore, JSlider slider) {
+    public WorkerThread(String name, BinarySemaphore binarySemaphore, JSlider slider) {
         super(name);
         this.binarySemaphore = binarySemaphore;
         this.slider = slider;
