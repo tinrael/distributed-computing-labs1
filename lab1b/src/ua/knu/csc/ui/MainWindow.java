0bc88399bde13f5079810613c988b3d10507a274
Timur Krasnopir <timur.krasnopir@unicyb.kiev.ua>
2021-02-13 12:55:57 +0200

    Replace local variables to fields

--- lab1b/src/ua/knu/csc/ui/MainWindow.java
+++ lab1b/src/ua/knu/csc/ui/MainWindow.java
@@ -9,6 +9,15 @@ import javax.swing.border.TitledBorder;
 import java.awt.*;
 
 public class MainWindow extends JFrame {
+    private final JSlider slider = new JSlider();
+
+    private final JButton buttonStart1 = new JButton("Start 1");
+    private final JButton buttonStop1 = new JButton("Stop 1");
+
+    private final JButton buttonStart2 = new JButton("Start 2");
+    private final JButton buttonStop2 = new JButton("Stop 2");
+
+    private final JLabel statusBarText = new JLabel("ready");
 
     public MainWindow(String title) {
         super(title);
@@ -36,7 +45,6 @@ public class MainWindow extends JFrame {
         JPanel viewPanel = new JPanel();
         viewPanel.setLayout(new FlowLayout());
 
-        JSlider slider = new JSlider();
         slider.setPaintTicks(true);
         slider.setPaintLabels(true);
         slider.setMajorTickSpacing(10);
@@ -54,7 +62,7 @@ public class MainWindow extends JFrame {
         statusBar.setLayout(new FlowLayout());
 
         statusBar.add(new JLabel("Status:"));
-        statusBar.add(new JLabel("ready"));
+        statusBar.add(statusBarText);
 
         return statusBar;
     }
@@ -65,15 +73,15 @@ public class MainWindow extends JFrame {
 
         Box controlBoxThread1 = Box.createVerticalBox();
         controlBoxThread1.setBorder(new CompoundBorder(new TitledBorder("Thread 1"), new EmptyBorder(5, 5, 5, 5)));
-        controlBoxThread1.add(new JButton("Start 1"));
+        controlBoxThread1.add(buttonStart1);
         controlBoxThread1.add(Box.createVerticalStrut(5));
-        controlBoxThread1.add(new JButton("Stop 1"));
+        controlBoxThread1.add(buttonStop1);
 
         Box controlBoxThread2 = Box.createVerticalBox();
         controlBoxThread2.setBorder(new CompoundBorder(new TitledBorder("Thread 2"), new EmptyBorder(5, 5, 5, 5)));
-        controlBoxThread2.add(new JButton("Start 2"));
+        controlBoxThread2.add(buttonStart2);
         controlBoxThread2.add(Box.createVerticalStrut(5));
-        controlBoxThread2.add(new JButton("Stop 2"));
+        controlBoxThread2.add(buttonStop2);
 
         controlPanel.add(controlBoxThread1);
         controlPanel.add(controlBoxThread2);
