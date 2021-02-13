c03006e55758bc00bb14b77d0028ea323ac406a0
Timur Krasnopir <timur.krasnopir@unicyb.kiev.ua>
2021-02-13 12:46:01 +0200

    Update the WorkerThread class

--- lab1b/src/ua/knu/csc/core/WorkerThread.java
+++ lab1b/src/ua/knu/csc/core/WorkerThread.java
@@ -3,25 +3,46 @@ package ua.knu.csc.core;
 import javax.swing.JSlider;
 
 public class WorkerThread extends Thread {
+    private final long THREAD_DELAY = 200000000;
+
     private final BinarySemaphore binarySemaphore;
 
     private final JSlider slider;
+    private final int targetSliderValue;
 
-    public WorkerThread(BinarySemaphore binarySemaphore, JSlider slider) {
+    public WorkerThread(BinarySemaphore binarySemaphore, JSlider slider, int targetSliderValue) {
         this.binarySemaphore = binarySemaphore;
         this.slider = slider;
+        this.targetSliderValue = targetSliderValue;
     }
 
-    public WorkerThread(String name, BinarySemaphore binarySemaphore, JSlider slider) {
+    public WorkerThread(String name, BinarySemaphore binarySemaphore, JSlider slider, int targetSliderValue) {
         super(name);
         this.binarySemaphore = binarySemaphore;
         this.slider = slider;
+        this.targetSliderValue = targetSliderValue;
     }
 
     @Override
     public void run() {
         try {
             binarySemaphore.acquire();
+
+            int currentSliderValue;
+            while (!isInterrupted()) {
+                currentSliderValue = slider.getValue();
+
+                if (currentSliderValue > targetSliderValue) {
+                    slider.setValue(currentSliderValue - 1);
+                } else if (currentSliderValue < targetSliderValue) {
+                    slider.setValue(currentSliderValue + 1);
+                } else {
+                    break;
+                }
+
+                long threadDelay = THREAD_DELAY;
+                while (threadDelay != 0) threadDelay--;
+            }
         } catch (InterruptedException e) {
             e.printStackTrace();
         } finally {
