f3b2db3ea76642342a4039a3793fd1a16cbc120e
Timur Krasnopir <timur.krasnopir@unicyb.kiev.ua>
2021-02-14 21:40:15 +0200

    Add the Truck class

---
+++ lab2b/src/ua/knu/csc/core/Truck.java
@@ -0,0 +1,37 @@
+package ua.knu.csc.core;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class Truck {
+    private final int capacity;
+    private int size = 0;
+
+    private final List<Item> items;
+
+    public Truck(int capacity) {
+        this.capacity = capacity;
+        this.items = new ArrayList<>(capacity);
+    }
+
+    public void addItem(Item item) {
+        if (size < capacity){
+            items.add(item);
+            size++;
+        }
+
+        throw new IllegalStateException("The truck is full.");
+    }
+
+    public boolean isFull() {
+        return size == capacity;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public int getSize() {
+        return size;
+    }
+}
