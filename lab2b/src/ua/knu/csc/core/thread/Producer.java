5cce392067a4a419a6eea6115ce453e664f5d269
Timur Krasnopir <timur.krasnopir@unicyb.kiev.ua>
2021-02-15 00:44:19 +0200

    Add the Producer class

---
+++ lab2b/src/ua/knu/csc/core/thread/Producer.java
@@ -0,0 +1,39 @@
+package ua.knu.csc.core.thread;
+
+import ua.knu.csc.core.entity.Item;
+import ua.knu.csc.core.Storage;
+import ua.knu.csc.core.entity.Warehouse;
+
+public class Producer extends Thread {
+    private final Storage storage;
+
+    private final Warehouse warehouse;
+
+    public Producer(String name, Storage storage, Warehouse warehouse) {
+        super(name);
+
+        if (storage == null) {
+            throw new NullPointerException("The specified storage is null.");
+        }
+
+        if (warehouse == null) {
+            throw new NullPointerException("The specified warehouse is null.");
+        }
+
+        this.storage = storage;
+        this.warehouse = warehouse;
+    }
+
+    @Override
+    public void run() {
+        try {
+            Item item;
+            while (!isInterrupted() && !warehouse.isEmpty()) {
+                item = warehouse.getItem();
+                storage.addItem(item);
+            }
+        } catch (InterruptedException e){
+            e.printStackTrace();
+        }
+    }
+}
