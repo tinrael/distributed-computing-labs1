1499b7528e5be9d78c34500bda45c13567577610
Timur Krasnopir <timur.krasnopir@unicyb.kiev.ua>
2021-02-15 00:44:34 +0200

    Add the Consumer class

---
+++ lab2b/src/ua/knu/csc/core/thread/Consumer.java
@@ -0,0 +1,39 @@
+package ua.knu.csc.core.thread;
+
+import ua.knu.csc.core.entity.Item;
+import ua.knu.csc.core.Storage;
+import ua.knu.csc.core.entity.Truck;
+
+public class Consumer extends Thread {
+    private final Storage storage;
+
+    private final Truck truck;
+
+    public Consumer(String name, Storage storage, Truck truck) {
+        super(name);
+
+        if (storage == null) {
+            throw new NullPointerException("The specified storage is null.");
+        }
+
+        if (truck == null) {
+            throw new NullPointerException("The specified truck is null.");
+        }
+
+        this.storage = storage;
+        this.truck = truck;
+    }
+
+    @Override
+    public void run() {
+        try {
+            Item item;
+            while (!isInterrupted() && !truck.isFull()) {
+                item = storage.getItem();
+                truck.addItem(item);
+            }
+        } catch (InterruptedException e){
+            e.printStackTrace();
+        }
+    }
+}
