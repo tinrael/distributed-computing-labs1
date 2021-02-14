2c33d9e6b3b8d84cc246ceaa86532e0d3f800c56
Timur Krasnopir <timur.krasnopir@unicyb.kiev.ua>
2021-02-14 21:39:37 +0200

    Add the Item class

---
+++ lab2b/src/ua/knu/csc/core/Item.java
@@ -0,0 +1,13 @@
+package ua.knu.csc.core;
+
+public class Item {
+    private final int cost;
+
+    public Item(int cost) {
+        this.cost = cost;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+}
