2f4b664dccafd6df8c0859a1f0a6acfee7ae1d3d
Timur Krasnopir <timur.krasnopir@unicyb.kiev.ua>
2021-03-02 16:51:15 +0200

    Update the Item class

--- lab2b/src/ua/knu/csc/core/entity/Item.java
+++ lab2b/src/ua/knu/csc/core/entity/Item.java
@@ -1,13 +1,24 @@
 package ua.knu.csc.core.entity;
 
 public class Item {
+    private final String name;
     private final int cost;
 
-    public Item(int cost) {
+    public Item(String name, int cost) {
+        this.name = name;
         this.cost = cost;
+    }
+
+    public String getName() {
+        return name;
     }
 
     public int getCost() {
         return cost;
+    }
+
+    @Override
+    public String toString() {
+        return "[name: " + name + ", cost: " + cost + "]";
     }
 }
