3e1095b9263afe0e033029e68677dcfbf0844317
Timur Krasnopir <timur.krasnopir@unicyb.kiev.ua>
2021-03-02 16:56:26 +0200

    Update the example

--- lab2b/src/ua/knu/csc/Main.java
+++ lab2b/src/ua/knu/csc/Main.java
@@ -6,32 +6,37 @@ import ua.knu.csc.core.entity.Truck;
 import ua.knu.csc.core.entity.Warehouse;
 
 import ua.knu.csc.core.thread.Consumer;
+import ua.knu.csc.core.thread.Middleman;
 import ua.knu.csc.core.thread.Producer;
 
 public class Main {
 
     public static void main(String[] args) throws InterruptedException {
-        Storage storage = new Storage();
+        Storage storage1 = new Storage("[producer - middleman]");
+        Storage storage2 = new Storage("[middleman - consumer]");
 
         // TODO: repair (?) the issue below.
         // The truck's capacity must be equal or more than the number of items in the warehouse (warehouse's size).
-        final int NUMBER_OF_ITEMS_IN_WAREHOUSE = 65;
+        final int NUMBER_OF_ITEMS_IN_WAREHOUSE = 70;
         final int TRUCK_CAPACITY = 100;
 
         Warehouse warehouse = new Warehouse(NUMBER_OF_ITEMS_IN_WAREHOUSE);
         Truck truck = new Truck(TRUCK_CAPACITY);
 
-        Producer producer = new Producer("Producer", storage, warehouse);
-        Consumer consumer = new Consumer("Consumer", storage, truck);
+        Producer producer = new Producer("Producer", storage1, warehouse);
+        Middleman middleman = new Middleman("Middleman", storage1, storage2);
+        Consumer consumer = new Consumer("Consumer", storage2, truck);
 
         System.out.println("Number of items in the WAREHOUSE: " + warehouse.getSize());
         System.out.println("Number of items in the TRUCK: " + truck.getSize());
         System.out.println();
 
         producer.start();
+        middleman.start();
         consumer.start();
 
         producer.join();
+        middleman.join();
         consumer.join();
 
         System.out.println("Number of items in the TRUCK: " + truck.getSize());
