ec8b2c80a73f804d1109f056f2340c38a52d3596
Timur Krasnopir <timur.krasnopir@unicyb.kiev.ua>
2021-04-06 14:34:18 +0300

    Add the initialize() method. Update the comments, the output text and the searchBear() function.
    Now the searchBear() function prints that the bear is found after the call to briefcase.close().

--- lab2a/lab2a.go
+++ lab2a/lab2a.go
@@ -9,16 +9,16 @@ import (
 
 type Briefcase struct {
 	mutex sync.Mutex
-	isEmpty bool // If true, then there are no more tasks.
-	numOfRows int // the overall number of tasks
-	nextRow int // the next task to do
+	isEmpty bool // If it is true, then there are no more tasks.
+	numOfRows int // The overall number of tasks. This number is supposed to be >= 0.
+	nextRow int // The next task to do. This number is supposed to be >= 0.
 }
 
-// Specifies that there are no more tasks.
-func (briefcase *Briefcase) close() {
-	briefcase.mutex.Lock()
-	briefcase.isEmpty = true
-	briefcase.mutex.Unlock()
+// This method has to be called before the use of a briefcase.
+func (briefcase *Briefcase) initialize() {
+	if briefcase.nextRow >= briefcase.numOfRows {
+		briefcase.isEmpty = true
+	}
 }
 
 // Returns the next task to do or if there are no more tasks returns -1.
@@ -40,6 +40,13 @@ func (briefcase *Briefcase) getNextRow() int {
 	return nextRow
 }
 
+// Specifies that there are no more tasks.
+func (briefcase *Briefcase) close() {
+	briefcase.mutex.Lock()
+	briefcase.isEmpty = true
+	briefcase.mutex.Unlock()
+}
+
 func searchBear(goroutineId int, forest [][]int, briefcase *Briefcase, waitGroup *sync.WaitGroup) {
 	defer waitGroup.Done()
 
@@ -47,17 +54,18 @@ func searchBear(goroutineId int, forest [][]int, briefcase *Briefcase, waitGroup
 		rowIndex := briefcase.getNextRow() // gets a task
 
 		if rowIndex == -1 { // if there are no more tasks
-			fmt.Printf("[%v]: Nothing to do. The group of bees [%v] returns to the hive.\n", goroutineId, goroutineId)
+			fmt.Printf("[%v]: There is nothing to do else. The group of bees [%v] returns to the hive.\n", goroutineId, goroutineId)
 			return
 		}
 
 		for columnIndex, value := range forest[rowIndex] {
 			if value == 1 { // if the bear is found
+				briefcase.close() // reports that the bear is found
 				fmt.Printf("[%v]: The bear is found at the location (%v, %v).\n", goroutineId, rowIndex, columnIndex)
-				briefcase.close() // reports that a bear is found
 				return
 			}
 		}
+
 		fmt.Printf("[%v]: The bear is not found in row %v.\n", goroutineId, rowIndex)
 	}
 }
@@ -80,6 +88,7 @@ func main() {
 	}
 
 	briefcase := Briefcase{numOfRows: len(forest)}
+	briefcase.initialize()
 
 	rand.Seed(time.Now().UnixNano())
 	randomRowIndex := rand.Intn(numOfRows)
@@ -88,7 +97,7 @@ func main() {
 	forest[randomRowIndex][randomColumnIndex] = 1 // places the bear at a random location in the forest
 	fmt.Printf("A bear is placed at the location (%v, %v).\n", randomRowIndex, randomColumnIndex)
 
-	fmt.Println("Here is the forest:")
+	fmt.Println("Here is a forest:")
 	for i := range forest {
 		fmt.Println(forest[i])
 	}
